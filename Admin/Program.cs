504b82e8f66e6e09d64f282ef93c93f62bc7cb6d
Rares Neagu <rares.neagu@ivanti.com>
2019-04-25 12:37:11 +0300

    Add files

---
+++ Admin/Program.cs
@@ -0,0 +1,74 @@
+﻿using Library;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Admin
+{
+    class Program
+    {
+        private static readonly object _lock = new object();
+        private static byte _volume = 100;
+        private static byte Volume
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _volume;
+                }
+            }
+            set
+            {
+                lock (_lock)
+                {
+                    _volume = value;
+                }
+            }
+        }
+        static void ClientThread(TcpClient client)
+        {
+            byte currentVolume = 0;
+            var stream = client.GetStream();
+            while (true)
+            {
+                try { 
+                Thread.Sleep(100);
+                var volume = Volume;
+                if (volume == currentVolume) continue;
+                currentVolume = volume;
+                Console.WriteLine("* " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString() + " sending: " + currentVolume);
+                stream.WriteByte(currentVolume);
+                } catch (Exception)
+                {
+                    Console.WriteLine("* " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString() + " client closed!");
+                    return;
+                }
+            }
+        }
+        static void Main()
+        {
+            Console.WriteLine("Starting server!");
+            var listener = new TcpListener(IPAddress.Any, 8123);
+            Console.WriteLine("Server ready!");
+            listener.Start();
+            new Thread(() => {
+                Thread.CurrentThread.IsBackground = true;
+                var volumeWatcher = new VolumeWatcher();
+                volumeWatcher.Change += (a) => Volume = a;
+                volumeWatcher.Watch();
+            }).Start();
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                Console.WriteLine("new client! " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    ClientThread(client);
+                }).Start();
+            }
+        }
+    }
+}
---
+++ User/App.cs
@@ -0,0 +1,87 @@
+﻿using Library;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace VolumeRocker
+{
+    internal class App
+    {
+        private const int PORT = 8123;
+        private readonly TcpClient _tcpClient;
+        private static readonly object _lock = new object();
+        private static byte _volume = 102;
+        private static byte Volume
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _volume;
+                }
+            }
+            set
+            {
+                lock (_lock)
+                {
+                    _volume = value;
+                    Console.WriteLine("Volume: " + _volume);
+                }
+            }
+        }
+
+        public App()
+        {
+            _tcpClient = new TcpClient();
+        }
+
+        internal void Run()
+        {
+            try 
+            { 
+                _tcpClient.Connect("ROM-L37556", PORT);
+                var stream = _tcpClient.GetStream();
+                new Thread(() =>
+                {
+                    try { 
+                    Thread.CurrentThread.IsBackground = true;
+                    var volumeWatcher = new VolumeWatcher();
+                    var i = 0;
+                    while (true)
+                    {
+                        if (++i == 100)
+                        {
+                            GC.Collect();
+                            i = 0;
+                        }
+                        Thread.Sleep(10);
+                        var volume = Volume;
+                        if (volume == 102) continue;
+                        if (volumeWatcher.Get() != volume)
+                        {
+                            volumeWatcher.Set(volume);
+                        }
+                    }
+                    } catch (Exception)
+                    {
+                        Console.WriteLine("closed connection?");
+                    }
+                }).Start();
+                while (true)
+                {
+                    var currentVolume = stream.ReadByte();
+                    if (currentVolume == -1)
+                    {
+                        Thread.Sleep(100);
+                        continue;
+                    }
+                    
+                    Volume = (byte)currentVolume;
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+        }
+    }
+}
